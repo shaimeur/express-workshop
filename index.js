00b869b475b337c27371167587a0f97115c9a3e8
shaimeur <haimeursaad@gmail.com>
2024-01-30 01:17:35 +0100

    adding the basic routing system with ease git add .

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require('express');
 const app = express();
 const port = 2670;
 
+app.get('/', (req, res) => {
+  // sending basic response!!
+  //   res.send('Hello from express!!');
+  res.status(200).json({
+    message: 'Hello from express server!!',
+    username: 'shaimeur',
+  });
+});
+
+app.post('/', (req, res) => {
+  res.send('Post request have been made !!');
+});
+
 app.listen(port, () => {
   console.log(`server is listening to ${port}....`);
 });
