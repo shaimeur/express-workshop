e209efb990828eb48111830a865d0cb3db5a2151
shaimeur <haimeursaad@gmail.com>
2024-01-31 01:53:34 +0100

    adding the get request with the convention of returning a object with the status results & data

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 2670;
 
-app.get('/', (req, res) => {
-  // sending basic response!!
-  //   res.send('Hello from express!!');
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    message: 'Hello from express server!!',
-    username: 'shaimeur',
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
   });
 });
-
-app.post('/', (req, res) => {
-  res.send('Post request have been made !!');
-});
+// getting data with streams and pipe :
+// app.get('/api/v1/tours', (req, res) => {
+//   const readeabale = fs.createReadStream('./dev-data/data/tours-simple.json');
+//   readeabale.pipe(res);
+// });
 
 app.listen(port, () => {
   console.log(`server is listening to ${port}....`);
