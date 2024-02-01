a64249ba412708993338711747031adc13de448b
shaimeur <haimeursaad@gmail.com>
2024-02-01 16:29:15 +0100

    adding the post requst and writing the file async

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 2670;
+// middleware
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -16,10 +18,37 @@ app.get('/api/v1/tours', (req, res) => {
     },
   });
 });
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        console.log(err);
+      }
+      res.status(201).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+          tours: tours,
+        },
+      });
+    }
+  );
+
+  // res.send('done');
+});
+
 // getting data with streams and pipe :
 // app.get('/api/v1/tours', (req, res) => {
 //   const readeabale = fs.createReadStream('./dev-data/data/tours-simple.json');
-//   readeabale.pipe(res);
+//   readeabale.pipe(res.status(201));
 // });
 
 app.listen(port, () => {
