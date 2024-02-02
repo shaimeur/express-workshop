21fea0ace1d4df00aeadd3434546149fbb43533b
shaimeur <haimeursaad@gmail.com>
2024-02-02 13:04:15 +0100

    adding the update endpoint

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 2670;
+
 // middleware
 app.use(express.json());
 
+// reading the tours-simple json file synchronouslly
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// get All tours
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -19,6 +22,26 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+// get one tour
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = +req.params.id;
+  const tour = tours.find((item) => item.id === id);
+  // res.send('sended!');
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: tour,
+    },
+  });
+});
+
+// post one new tour
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
@@ -42,22 +65,37 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
-app.get('/api/v1/tours/:id', (req, res) => {
+// update a tour
+
+app.put('/api/v1/tours/:id', (req, res) => {
   const id = +req.params.id;
+
   const tour = tours.find((item) => item.id === id);
-  // res.send('sended!');
-  if (!tour) {
+  // console.log(tour);
+  // console.log(req.body);
+  if (id > tours.length) {
     return res.status(404).json({
       status: 'fail',
       message: 'invalid ID',
     });
   }
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: tour,
-    },
-  });
+  const updateTour = [...tours, Object.assign(tour, req.body)];
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(updateTour),
+    (err) => {
+      if (err) {
+        console.log(err);
+      }
+      res.status(200).json({
+        status: 'success',
+        data: {
+          updateTour,
+        },
+      });
+    }
+  );
 });
 
 // getting data with streams and pipe :
