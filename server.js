876f085b01cb428f80dcd8a30f17872e93b2e9bd
shaimeur <haimeursaad@gmail.com>
2024-04-21 17:37:45 +0100

    adding the configuration to our remote database using mongoAtlass and dotenv package

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
-require('dotenv').config();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const port = process.env.PORT || 2670;
 const host = '127.0.0.1';
 const app = require('./index');
+
+dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('Database connected successfully');
+  })
+  .catch((err) => {
+    console.log(err, 'Database connection failed');
+  });
 
 console.log(app.get('env'));
 // console.log(process.env.USERNAM);
@@ -9,7 +29,3 @@ console.log(app.get('env'));
 app.listen(port, host, () => {
   console.log(`server is listening to http://${host}:${port}`);
 });
-
-const x = 66;
-
-x = 0;
