79e2639eaa4ba82d3f1e458435e7713c2f0748a2
shaimeur <haimeursaad@gmail.com>
2024-04-22 00:13:49 +0100

    adding the tourSchema and its model using the mongoose api

--- server.js
+++ server.js
@@ -23,6 +23,23 @@ mongoose
     console.log(err, 'Database connection failed');
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 console.log(app.get('env'));
 // console.log(process.env.USERNAM);
 
